e57c350523976db13718682038a3c2becdac0904
dkdkdsa <bos94330@gmail.com>
2023-11-06 12:34:52 +0900

    Weapon

--- Assets/02_Script/Weapon/Weapon.cs
+++ Assets/02_Script/Weapon/Weapon.cs
@@ -1,3 +1,4 @@
+using FD.Dev;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,29 @@ using UnityEngine;
 public abstract class Weapon : MonoBehaviour
 {
 
-    
+    [SerializeField] protected WeaponDataSO _data;
+
+    protected bool _isCoolDown { get; private set; }
+
+    public void CastingWeapon(Transform target, float range)
+    {
+
+        if (_data.Range > range) return;
+        if (_isCoolDown) return;
+
+        _isCoolDown = true;
+
+        FAED.InvokeDelay(() =>
+        {
+
+            _isCoolDown = false;
+
+        }, _data.AttackCoolDown);
+
+        DoAttack();
+
+    }
+
+    protected abstract void DoAttack();
 
 }
--- Assets/02_Script/Weapon/WeaponDataSO.cs
+++ Assets/02_Script/Weapon/WeaponDataSO.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class WeaponDataSO : ScriptableObject
 {
     
-
+    [field: SerializeField] public float Range { get; protected set; }
+    [field: SerializeField] public float AttackPower { get; protected set; }
+    [field: SerializeField] public float AttackCoolDown { get; protected set; }
 
 }
