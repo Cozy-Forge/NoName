2c198122520cada186e947894dc4f8deaac12595
dkdkdsa <bos94330@gmail.com>
2023-11-12 15:33:22 +0900

    Weapon

--- Assets/02_Script/Weapon/Weapon.cs
+++ Assets/02_Script/Weapon/Weapon.cs
@@ -6,9 +6,17 @@ using UnityEngine;
 public abstract class Weapon : MonoBehaviour
 {
 
-    [SerializeField] protected WeaponDataSO _data;
+    [SerializeField] protected WeaponDataSO _so;
+    protected WeaponData _data;
 
     protected bool _isCoolDown { get; private set; }
+
+    protected virtual void Awake()
+    {
+        
+        _data = _so.Data;
+
+    }
 
     public void CastingWeapon(Transform target, float range)
     {
