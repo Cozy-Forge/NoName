57a5ca435d1b0db2286230ee6b37531daf6593bc
Wizone06 <haemjjiyul548@gmail.com>
2023-11-13 12:26:31 +0900

    근접무기 휘두르기
    근접무기 휘두르기

--- Assets/02_Script/Weapon/MeleeWeapon.cs
+++ Assets/02_Script/Weapon/MeleeWeapon.cs
@@ -1,15 +1,36 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeWeapon : Weapon
 {
+
+    public float _swingTime = 0.2f;
+    public float _swingDelayTime = 1f;
+  
     protected override void DoAttack(Transform trm)
     {
-
         Debug.Log("근접공격");
 
+        StartCoroutine(Swing());
     }
-       
+
+    private IEnumerator Swing()
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation1 = Quaternion.Euler(0, 0, 40) * startRotation;
+
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime / _swingTime;
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation1, t);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(_swingDelayTime);
+
+        StopCoroutine(Swing());
+    }
+
 }
