1a5ca085cbcc5b4e44fe783c60222833ab6ad820
dkdkdsa <bos94330@gmail.com>
2023-11-14 01:35:27 +0900

    Feedback

---
+++ Assets/02_Script/Feedback/Editor/FeedbackEditor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FeedbackPlayer))]
+public class FeedbackEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+
+        base.OnInspectorGUI();
+
+
+
+    }
+
+}
---
+++ Assets/02_Script/Feedback/FeedbackPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Flags]
+public enum FeedbackFlag
+{
+
+    None = 0,
+    HitShake = 4,
+
+}
+
+public class FeedbackPlayer : MonoBehaviour
+{
+    
+
+
+}
