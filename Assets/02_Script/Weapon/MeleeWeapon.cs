2d1f9046501ffca4710c147f99a4cd7bed0c8756
Wizone06 <haemjjiyul548@gmail.com>
2023-11-13 11:03:42 +0900

    중거리 원거리 근접 완료
    중거리 원거리 근접 완료

--- Assets/02_Script/Weapon/MeleeWeapon.cs
+++ Assets/02_Script/Weapon/MeleeWeapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,5 @@ public class MeleeWeapon : Weapon
         Debug.Log("근접공격");
 
     }
-
-
+       
 }
