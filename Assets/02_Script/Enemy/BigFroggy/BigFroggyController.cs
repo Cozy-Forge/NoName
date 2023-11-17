ba180ce35a9cbdff0c6939511056c20b74eced48
dkdkdsa <bos94330@gmail.com>
2023-11-18 00:15:54 +0900

    BigFroggyController

--- Assets/02_Script/Enemy/BigFroggy/BigFroggyController.cs
+++ Assets/02_Script/Enemy/BigFroggy/BigFroggyController.cs
@@ -29,16 +29,20 @@ public class BigFroggyController : StateController<EnumBigFroggyState>
             (transform, _data.JumpRange, _data.TargetAbleLayer, EnumBigFroggyState.Skill_Jump, () => !_data.IsJumpCoolDown);
 
         var idleToFire = new EnemyTargetRangeTransition<EnumBigFroggyState>
-            (transform, _data.JumpRange, _data.TargetAbleLayer, EnumBigFroggyState.Skill_Jump, () => !_data.IsJumpCoolDown);
+            (transform, _data.JumpRange, _data.TargetAbleLayer, EnumBigFroggyState.Skill_Fire, () => !_data.IsFireCoolDown);
 
-        idleState.AddTransition(idleToJump);
+        idleState
+            .AddTransition(idleToJump)
+            .AddTransition(idleToFire);
 
        #endregion
 
         var jumpState = new BigFroggyJumpState(this, _data);
+        var fireState = new BigFroggyFireState(this, _data);
 
         _stateContainer.Add(EnumBigFroggyState.Idle, idleState);
         _stateContainer.Add(EnumBigFroggyState.Skill_Jump, jumpState);
+        _stateContainer.Add(EnumBigFroggyState.Skill_Fire, fireState);
 
         CurrentState = EnumBigFroggyState.Idle;
 
