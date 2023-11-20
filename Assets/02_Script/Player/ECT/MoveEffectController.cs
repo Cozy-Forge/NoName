7da5ccc0c1572c357a6e415ff5e37b4fa75471b9
dkdkdsa <bos94330@gmail.com>
2023-11-21 08:37:40 +0900

    MEC

--- Assets/02_Script/Player/ECT/MoveEffectController.cs
+++ Assets/02_Script/Player/ECT/MoveEffectController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveParticleController : MonoBehaviour
+public class MoveEffectController : MonoBehaviour
 {
 
     private PlayerController _controller;
